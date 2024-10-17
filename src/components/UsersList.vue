09d8d2f24530ea1572d483aacc6e2e540f7e699f
Mary <maryqueenk1@gmail.com>
2024-10-17 19:57:28 +0300

    list of users

---
+++ src/components/UsersList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Список пользователей</h1>
+    <ul>
+      <!-- перебор пользователей -->
+      <li v-for="user in users" :key="user.id">
+        <!-- ссылка на страницу конкретного пользователя -->
+        <router-link :to="`/user/${user.id}`">
+          {{ user.name }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "UsersList",
+  computed: {
+    ...mapState(["users"]), //состояние пользователей из Vuex
+  },
+  created() {
+    this.$store.dispatch("fetchUsers"); // вызов загрузки users при создании компонента
+  },
+};
+</script>
+
+<style scoped lang="scss">
+ul {
+    list-style-type: none; // убираем маркеры у списка
+}
+</style>
